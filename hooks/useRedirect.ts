e7315a48d93f16baaad08476460b8c3b7e1b7ed4
MrFelopes <feliperfragoso@gmail.com>
2025-08-29 18:29:26 -0300

    refactor: fix useRedirectIfTokenIsValid hook and rename useAuth.ts to useRedirect.ts

---
+++ hooks/useRedirect.ts
@@ -0,0 +1,17 @@
+﻿"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { isTokenExpired } from "@/utils/auth/auth";
+import toast from "react-hot-toast";
+
+export function useRedirectIfTokenIsValid(): void
+{
+    const router = useRouter();
+    useEffect(() => {
+        const token = localStorage.getItem("authToken");
+        if (token && !isTokenExpired(token)) {
+            router.replace('/');
+            toast.error("Você já está autenticado!")
+        }
+    }, [router]);
+}
